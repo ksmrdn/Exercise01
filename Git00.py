baac97cc2d058f5eee8597d1074076b48c12bf7c
ksmrdn <kusumardanar@gmail.com>
2019-11-07 10:20:59 +0700

    Comment lagi

--- Git00.py
+++ Git00.py
@@ -4,12 +4,12 @@ print('Rachmad pasti bisa!')
 #commit --> commit
 #git add . -> semua
 #git add nama_file.py
-#git status --> cek status
+#git status --> cek status --> cek dulu sebelum melakukan apa-apa
 #git rm --cached nama_file --> hapus file
 #git commit -m "My first commit"
 #git log --> ada berapa commit
 #git log --oneline --> singkat
-
+#add --> commit --> log
 # A --> Siap di commit
 # U --> Belum siap di commit
 # M --> Modify
