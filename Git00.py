73417f8a189376095f77eeaf8475b64ff60cf3f9
ksmrdn <kusumardanar@gmail.com>
2019-11-07 10:16:31 +0700

    00 tambah comment, 01 tambah if clause

--- Git00.py
+++ Git00.py
@@ -3,4 +3,13 @@ print('Rachmad pasti bisa!')
 #staging area --> add
 #commit --> commit
 #git add . -> semua
-#git add nama_file
+#git add nama_file.py
+#git status --> cek status
+#git rm --cached nama_file --> hapus file
+#git commit -m "My first commit"
+#git log --> ada berapa commit
+#git log --oneline --> singkat
+
+# A --> Siap di commit
+# U --> Belum siap di commit
+# M --> Modify
--- Git01.py
+++ Git01.py
@@ -1,1 +1,7 @@
 print('Iya Rachmad bisa!')
+
+x = 16
+if x >= 15:
+    print ('Sudah ABG')
+else:
+    print('Belum ABG')
